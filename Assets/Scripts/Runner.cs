ba840dd13bb3854faac7e499eb8714d1488fefb9
TXMAY <juya6388king@naver.com>
2024-09-10 19:47:21 +0900

    Day 1 - Init

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoadLine
+{
+    LEFT = -1,
+    MIDDLE,
+    RIGHT,
+}
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] RoadLine roadLine;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
