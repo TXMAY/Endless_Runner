2a4884e52b66144e7b019eba3c3b553eeac1ed4e
TXMAY <juya6388king@naver.com>
2024-09-11 19:44:12 +0900

    Day 2 - Rigidbody move, Lerp, LoadScene

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -12,13 +12,49 @@ public enum RoadLine
 public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadLine;
+    [SerializeField] Rigidbody rigidbody;
+
+    [SerializeField] float speed = 25.0f;
+    [SerializeField] float positionX = 2.5f;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
     void Start()
     {
+        roadLine = RoadLine.MIDDLE;
+    }
 
+    void OnKeyUpdate()
+    {
+        if (roadLine != RoadLine.LEFT && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            roadLine--;
+        }
+        if (roadLine != RoadLine.RIGHT && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            roadLine++;
+        }
+    }
+
+    void Move()
+    {
+        rigidbody.position = Vector3.Lerp
+        (
+            rigidbody.position,
+            new Vector3(positionX * (int)roadLine, transform.position.y),
+            speed * Time.fixedDeltaTime
+        );
     }
 
     void Update()
     {
+        OnKeyUpdate();
+    }
 
+    void FixedUpdate()
+    {
+        Move();
     }
 }
